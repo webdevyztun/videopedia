0d43f55ca3cbee82829fc9843093717f4db79f5d
Fall Lay <webdevyztun@gmail.com>
2016-04-07 21:49:14 +0630

    stylesheet

--- index.php
+++ index.php
@@ -2,9 +2,30 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE-edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Video Pedia</title>
+
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
-	
+	<div class="container">
+		<div class="row">
+			<h3>Video Pedia</h3>
+		</div>
+	</div>
+</div>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.dataTables.min.js"></script>
+<script src="js/dataTables.bootstrap.js"></script>
+
+<script>
+	$(document).ready(function(){
+		
+	});
+</script>
 </body>
 </html>
